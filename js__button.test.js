e91cb48d9cff1f57f5e873c3dfe40ae2ace6a9f9
iamgallagher <gallagher.elizabeth007@gmail.com>
2020-12-17 15:10:47 -0600

    Tried to run a test

---
+++ js__button.test.js
@@ -0,0 +1,12 @@
+/* const button = require("./js__button");
+
+test("changesBackground", () => {
+  expect(button).toBe(changeBackground);
+});
+
+>> Throwing an error. Not a good test. 
+
+
+
+
+*/
